a533e8537e0df215c8fdbcd90595c47cf01c7c59
LittleSheep <hi@littlesheep.me>
2026-08-03 02:30:16 +0800

    :bug: Fix local auth

--- android/app/src/main/kotlin/com/example/maid_kit/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/maid_kit/MainActivity.kt
@@ -1,5 +1,5 @@
 package dev.solsynth.maid
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
